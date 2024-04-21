4ffe15e5ec61bc2dabe8cddbfb768ca5031f4f91
PaulaTam <ptam4@sfsu.edu>
2024-04-21 15:57:53 -0700

    Add onClick attribute to Button

--- src/js/Button.js
+++ src/js/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = (props) => {
   return (
-    <button type={props.type} name={props.name} className={props.class}>
+    <button type={props.type} name={props.name} className={props.class} onClick={props.onclick}>
       {props.text} {/*text on button*/}
     </button>
   );
