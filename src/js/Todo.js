10717a76f7d499cdcd2481baa603597afeae8467
Camille MacLaren <camillemaclaren@gmail.com>
2024-04-28 18:20:56 -0500

    display basic todo list html elements

--- src/js/Todo.js
+++ src/js/Todo.js
@@ -1,9 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Todo = () => {
-  // init lists state
-  //const [listsState, setListsState] = useState();
-
   // get saved list from storage & convert to json
   let storedLists = localStorage.getItem('todo_lists');
   let lists = JSON.parse(storedLists);
@@ -22,14 +19,34 @@ const Todo = () => {
 
   if (!storedLists) {
     localStorage.setItem('todo_lists', JSON.stringify(default_lists));
+    lists = default_lists;
   } else {
-    lists;
+    console.log(lists);
   }
+  // init lists state
+  const [listsState, setListsState] = useState(lists);
 
   //html
   return (
     <div>
       <p> This is the todo list area</p>
+      <form className="todolists">
+        {listsState.map((l, ind) => (
+          <div className="list-wrapper">
+            <div>{l.list_name}</div>
+            {l.tasks.map((task) => (
+              <div>
+                <input
+                  className="task-input"
+                  type="checkbox"
+                  name="task_check"
+                  checked={task.checked}></input>
+                <input value={task.task_name}></input>
+              </div>
+            ))}
+          </div>
+        ))}
+      </form>
     </div>
   );
 };
