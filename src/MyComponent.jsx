206b44bf6288f8725cf2d00921c9483c1dd7f1f0
Yanka Sikder <sikderyanka@gmail.com>
2024-04-28 21:00:48 -0400

    freeJournalEdit

---
+++ src/MyComponent.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+function MyComponent() {
+  const [value, setValue] = useState('');
+  var toolbarOptions = [['bold', 'italic'], ['link', 'image']];
+  const module = {
+    toolbar: toolbarOptions,
+  };
+
+  return <ReactQuill modules {module} theme="snow" value={value} onChange={setValue} />;
+}
+
+export default MyComponent;
--- src/pages/FreeJournal.js
+++ src/pages/FreeJournal.js
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Button from '../js/Button.js';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const FreeJournal = () => {
+  const fillerFunc = () => {
+    console.log('button test');
+  };
+
+  const clearFunc = () => {
+    console.log('Clearing the entry button');
+    setValue('');
+  };
+
+  const [value, setValue] = useState('');
+  const [title, setTitle] = useState('');
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value); // Updates the title as the user types
+  };
+  const Font = ReactQuill.Quill.import('formats/font');
+  Font.whitelist = ['mirza', 'roboto'];
+  ReactQuill.Quill.register(Font, true);
+
   return (
-    <div>
-      <header>This is the temporary free journal page</header>
-      <Link to="/">go back</Link>
+    <div
+      className="journal-entry"
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h1 className="text-3xl font-bold underline" style={{ textAlign: 'center' }}>
+        Free Flowing Journal
+      </h1>
+      <h2 className="text-1xl font-bold" style={{ textAlign: 'center' }}>
+        Let your words flow!
+      </h2>
+
+      <div
+        style={{
+          backgroundColor: '#F6EFDE',
+          boxShadow: '0 4px 6px rgba(0,0,0,0.1)',
+          borderRadius: '10px',
+          width: '100%',
+          maxWidth: '1000px',
+          padding: '20px',
+          margin: '20px auto' // This will center the div and maintain 20px margin from top and bottom
+        }}>
+        <input
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+          placeholder="Enter Title of this Journal Entry..."
+          className="title-input"
+          style={{
+            width: '100%',
+            padding: '5px',
+            fontSize: '20px',
+            margin: '10px 0',
+            display: 'block',
+            backgroundColor: '#F6EFDE'
+          }}
+        />
+        <ReactQuill theme="snow" value={value} onChange={setValue} />
+      </div>
+      <Button type="button" name="clear" text="Clear" class="btn btn-primary" onclick={clearFunc} />
+      <Button type="button" name="save" text="Save" class="btn btn-primary" onclick={fillerFunc} />
+      <Link to="/">Go Back</Link>
     </div>
   );
 };
