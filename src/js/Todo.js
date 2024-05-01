e5cb4f7483603f6268d77c4c795d8ae50cc1b4ad
Camille MacLaren <camillemaclaren@gmail.com>
2024-05-01 16:06:25 -0500

    Update Todolist.js to have unique ids
    - add the uuid npm package
    - all functions now operate on finding the specific ids of the tasks and lists when edited, saved, and deleted
    - previous version found the index by the html attributes which might not be reliable
    - removed the eslint rule that allowed custom attributes

--- src/js/Todo.js
+++ src/js/Todo.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const Todo = () => {
   // get saved list from storage & convert to json
   let storedLists = JSON.parse(localStorage.getItem('todo_lists'));
   let lists = storedLists;
-  //default json for empty storage
+  // default json for empty storage
   let default_lists = {
     lists: [
       {
         list_name: '',
+        list_id: uuidv4(),
         tasks: [
           {
             checked: false,
-            task_name: ''
+            task_name: '',
+            task_id: uuidv4()
           }
         ]
       }
@@ -20,43 +23,44 @@ const Todo = () => {
   };
 
   if (!storedLists) {
+    // if there is no store info save default into local storage
     localStorage.setItem('todo_lists', JSON.stringify(default_lists));
+    // set default to current lists
     lists = default_lists;
   }
 
   // init lists state
   const [listsState, setListsState] = useState(lists);
 
-  const onListChange = (e) => {
+  const onListChange = (e, list_id) => {
+    // get info on event target
     const { name, value } = e.target;
-    const indL = e.target.getAttribute('indexlist');
-
+    // make temp object of lists
     const lists = [...listsState.lists];
-    lists[indL] = {
-      // keep info to the specific list
-      ...listsState.lists[indL],
-      //set new info
+    let listIndex = lists.findIndex((x) => x.list_id === list_id);
+    // edit that specific event
+    lists[listIndex] = {
+      ...listsState.lists[listIndex],
       [name]: value
     };
+    // change listState to the temp list
     setListsState({ lists });
   };
 
-  const onTaskChange = (e) => {
+  const onTaskChange = (e, list_id, task_id) => {
     const { name, value, checked } = e.target;
-    const indL = e.target.getAttribute('indexlist');
-    const indT = e.target.getAttribute('indextask');
-
     const lists = [...listsState.lists];
+    const listIndex = lists.findIndex((x) => x.list_id === list_id);
+    const taskIndex = lists[listIndex].tasks.findIndex((x) => x.task_id === task_id);
+
     if (name == 'task_name') {
-      lists[indL].tasks[indT] = {
-        // keep info to the specific set
-        ...listsState.lists[indL].tasks[indT],
-        //set new info
+      lists[listIndex].tasks[taskIndex] = {
+        ...listsState.lists[listIndex].tasks[taskIndex],
         [name]: value
       };
     } else {
-      lists[indL].tasks[indT] = {
-        ...listsState.lists[indL].tasks[indT],
+      lists[listIndex].tasks[taskIndex] = {
+        ...listsState.lists[listIndex].tasks[taskIndex],
         [name]: checked
       };
     }
@@ -67,89 +71,97 @@ const Todo = () => {
     const lists = [...listsState.lists];
     lists.push({
       list_name: '',
+      list_id: uuidv4(),
       tasks: [{ task_name: '', checked: false }]
     });
     setListsState({ lists });
   };
 
-  const addTask = (indL) => {
+  const addTask = (list_id) => {
     const lists = [...listsState.lists];
-    lists[indL].tasks.push({ task_name: '', checked: false });
+    const listIndex = lists.findIndex((x) => x.list_id === list_id);
+    lists[listIndex].tasks.push({ task_name: '', checked: false, task_id: uuidv4() });
     setListsState({ lists });
   };
 
-  const deleteList = (indL) => {
+  const deleteList = (list_id) => {
     const lists = [...listsState.lists];
-    lists.splice(indL, 1);
+    const index = lists.findIndex((x) => x.list_id === list_id);
+    lists.splice(index, 1);
     setListsState({ lists });
   };
 
-  const deleteTask = (indL, indT) => {
+  const deleteTask = (list_id, task_id) => {
     const lists = [...listsState.lists];
-    lists[indL].tasks.splice(indT, 1);
+    const listIndex = lists.findIndex((x) => x.list_id === list_id);
+    const taskIndex = lists[listIndex].tasks.findIndex((x) => x.task_id === task_id);
+    lists[listIndex].tasks.splice(taskIndex, 1);
     setListsState({ lists });
   };
 
   const handleFormSubmit = (e) => {
-    // prevents form submitting to itself
     e.preventDefault();
     // saves to local storage
     localStorage.setItem('todo_lists', JSON.stringify(listsState));
   };
 
-  window.addEventListener('beforeunload', () => {
-    // auto save when going to other site (does nt work within site routes)
-    localStorage.setItem('todo_lists', JSON.stringify(listsState));
-  });
+  // window.addEventListener('beforeunload', () => {
+  //   // auto save when going to other site (does nt work within site routes)
+  //   localStorage.setItem('todo_lists', JSON.stringify(listsState));
+  // });
 
   //html
   return (
     <div>
       <p> This is the todo list area</p>
-      
+
       <form className="todolists" onSubmit={handleFormSubmit}>
         <button onClick={addList}>Add List</button>
         <br />
         <br />
-        {listsState.lists.map((list, indL) => (
-          <div key={indL} className="list-wrapper">
+        {listsState.lists.map((list) => (
+          <div key={list.list_id} className="list-wrapper">
             <input
               type="text"
               name="list_name"
               value={list.list_name}
               placeholder="Enter list name"
-              indexlist={indL}
-              onChange={onListChange}></input>
+              onChange={(event) => {
+                event.preventDefault();
+                onListChange(event, list.list_id);
+              }}></input>
             <br />
             <button
               onClick={(event) => {
                 event.preventDefault();
-                deleteList(indL);
+                deleteList(list.list_id);
               }}>
               Delete List
             </button>
-            {list.tasks.map((task, indT) => (
-              <div key={indT} className="task-wrapper">
+            {list.tasks.map((task) => (
+              <div key={task.task_id} className="task-wrapper">
                 <input
                   className="task-input"
                   type="checkbox"
                   name="checked"
                   checked={task.checked}
-                  indexlist={indL}
-                  indextask={indT}
-                  onChange={onTaskChange}></input>
+                  onChange={(event) => {
+                    event.preventDefault();
+                    onTaskChange(event, list.list_id, task.task_id);
+                  }}></input>
                 <input
                   type="text"
                   name="task_name"
                   value={task.task_name}
                   placeholder="Enter task name"
-                  indexlist={indL}
-                  indextask={indT}
-                  onChange={onTaskChange}></input>
+                  onChange={(event) => {
+                    event.preventDefault();
+                    onTaskChange(event, list.list_id, task.task_id);
+                  }}></input>
                 <button
                   onClick={(event) => {
                     event.preventDefault();
-                    deleteTask(indL, indT);
+                    deleteTask(list.list_id, task.task_id);
                   }}>
                   Delete Task
                 </button>
@@ -158,7 +170,7 @@ const Todo = () => {
             <button
               onClick={(event) => {
                 event.preventDefault();
-                addTask(indL);
+                addTask(list.list_id);
               }}>
               Add Task
             </button>
