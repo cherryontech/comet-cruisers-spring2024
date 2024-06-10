6388f2bf8ae55c98b8d693b69f95f205526ae9f1
PaulaTam <ptam4@sfsu.edu>
2024-06-09 19:14:12 -0700

    Added icons and css to modals

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,10 @@ import { IoMdClose } from 'react-icons/io';
 import 'reactjs-popup/dist/index.css';
 import DisplayJournal from '../js/displayEntries.js';
 import logo from '../assets/Logo-Sun.png';
+import light_bulb from '../assets/LightBulb.svg';
+import pencil from '../assets/Pencil.svg';
 import GenerateMessage from '../js/botPrompt.js';
-import Tutorial from '../js/Tutorial';
+import Tutorial from '../js/Tutorial.js';
 
 const Home = () => {
   const displayMascot = () => {
@@ -41,17 +43,26 @@ const Home = () => {
               {(close) => (
                 <div className="modal p-4">
                   <div className="content flex flex-row">
-                    Choose
+                    <div className="flex-col">
+                      <h1 className="text-xl">How would you like to journal today?</h1>
+                      <p>Pick your journaling activity for today.</p>
+                    </div>
                     <button className="grow" onClick={() => close()}>
                       <IoMdClose className="float-right" />
                     </button>
                   </div>
-                  <div className="flex flex-row gap-3 mt-4">
-                    <button className="flex-1">
-                      <Link to="/free-journal">Free Write</Link>
+                  <div className="flex flex-row center gap-3 mt-4">
+                    <button className="flex popup-btn hover justify-center">
+                      <div className="flex flex-col gap-y-3">
+                        <img src={pencil} alt="Free Write Icon" className="w-32 h-32" />
+                        <Link to="/free-journal">Free Write</Link>
+                      </div>
                     </button>
-                    <button className="flex-1">
-                      <Link to="/prompt-journal">Random Prompt</Link>
+                    <button className="flex popup-btn hover justify-center">
+                      <div className="flex flex-col gap-y-3">
+                        <img src={light_bulb} alt="Prompt Icon" className="w-32 h-32" />
+                        <Link to="/prompt-journal">Random Prompt</Link>
+                      </div>
                     </button>
                   </div>
                 </div>
