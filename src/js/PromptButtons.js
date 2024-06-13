f3c5c2a90eb2d4a85c07d36edd35649b5fc77ddb
PaulaTam <ptam4@sfsu.edu>
2024-06-13 15:11:10 -0700

    Added unsaved popup when switching journals

--- src/js/PromptButtons.js
+++ src/js/PromptButtons.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Popup from 'reactjs-popup';
 import { GoLightBulb } from 'react-icons/go';
 import { TbWriting } from 'react-icons/tb';
 
@@ -30,9 +31,30 @@ const SwitchJournalType = () => {
   };
 
   return (
-    <button className="switch-btn" title="switch journal type">
-      <Link to={switchTo}> {displayIcon()} </Link>
-    </button>
+    <Popup
+      contentStyle={{ backgroundColor: '#F6EFDE', borderColor: '#E36527' }}
+      trigger={
+        <button className="switch-btn" title="switch journal type">
+          {displayIcon()}
+        </button>
+      }
+      modal
+      nested>
+      {(close) => (
+        <div className="modal p-4">
+          <h1 className="text-2xl font-semibold">Unsaved Changes</h1>
+          <p>Are you sure you want to change journal types?</p>
+          <div className="flex flex-row justify-center gap-5 pt-4">
+            <button className="btn btn-secondary hover flex-1" onClick={() => close()}>
+              Cancel
+            </button>
+            <Link to={switchTo} className="flex flex-1">
+              <button className="btn btn-tertiary hover flex-1">Switch!</button>
+            </Link>
+          </div>
+        </div>
+      )}
+    </Popup>
   );
 };
 
